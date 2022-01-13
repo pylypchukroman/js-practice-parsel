96c6ed92fe8a3b24a1d84b975ae5e8e8befe1d1a
pylypchukroman <romanpylypchuk@gmail.com>
2022-01-13 22:38:43 +0200

    up

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,30 +1,34 @@
 // console.log("OK - 2");
 const refs = {
-    menuList: document.querySelector('.menu__list'),
-    mainEl: document.querySelector('main'),
-}
+  menuList: document.querySelector('.menu__list'),
+  imageList: document.querySelector('main ul')
+};
+
 const baseUrl = 'https://rickandmortyapi.com/api';
 
- function getDataServer(url) {
-     return fetch(url)
-         .then(response => response.json());
- }
+function getDataServer(url) {
+  return fetch(url).then(response => response.json());
+}
 
 getDataServer(baseUrl).then(data => {
-    const markup = Object.entries(data).map(([key, value]) =>
-        `<li><a href="${value}">${key}</a></li>`).join('');
+  const markup = Object.entries(data)
+    .map(([key, value]) => `<li><a href="${value}">${key}</a></li>`)
+    .join('');
 
-
-    refs.menuList.innerHTML = markup;
+  refs.menuList.innerHTML = markup;
 });
 
-document.addEventListener("click", event => {
-    if (event.target.nodeName !== 'A') return;
-    event.preventDefault();
-    getDataServer(event.target.href).then(data => {
-        const markupImage = data.results.map(character =>
-            `<li><img src="${character.image}></img></li>`).join('');
-        refs.mainEl.innerHTML = markupImage;
-    });
-});
+document.addEventListener('click', event => {
+  if (event.target.nodeName !== 'A') return;
+  event.preventDefault();
+  getDataServer(event.target.href).then(data => {
+    const markupImage = data.results
+      .map(character => `<li class="image"><img src="${character.image}"></li>`)
+      .join('');
+    refs.imageList.innerHTML = markupImage;
 
+
+  })
+
+
+});
