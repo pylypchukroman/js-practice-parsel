a09ece37eaaa6deb2b1fcc4908a3ce6e62946250
pylypchukroman <romanpylypchuk@gmail.com>
2022-01-16 17:33:27 +0200

    up

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -1,1 +1,19 @@
 console.log("OK - 3");
+
+
+const counter = document.querySelector('.counter');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', onButtonClick);
+
+const promice = fetch('https://jsonplaceholder.typicode.com/todos/3/todos')
+    .then(response => response.json());
+
+promice.then(data => console.log(data));
+
+function onButtonClick() {
+    promice.then(data => data.map((user) => console.log(user.title)))
+
+
+    promice.then(data => counter.textContent = data.title);
+}
